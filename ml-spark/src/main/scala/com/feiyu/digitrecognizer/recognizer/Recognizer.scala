7d5c93db8a41093244533b9f50d743dd04b86d7d
faustineinsun <faustine.yu@gmail.com>
2016-05-13 10:01:32 -0700

    Resize image to fit model

--- ml-spark/src/main/scala/com/feiyu/digitrecognizer/recognizer/Recognizer.scala
+++ ml-spark/src/main/scala/com/feiyu/digitrecognizer/recognizer/Recognizer.scala
@@ -1,5 +1,6 @@
 package com.feiyu.digitrecognizer.recognizer
 
+import java.awt.Image
 import java.awt.image.{BufferedImage, Raster}
 import java.io.File
 import javax.imageio.ImageIO
@@ -18,25 +19,33 @@ object Recognizer {
       .setAppName("DigitRecognizerJar")
       .setMaster("local")
     val sc = new SparkContext(conf)
-    val image: BufferedImage = ImageIO.read(new File("src/main/resources/data/2.png"))
-    val raster: Raster  = image.getRaster()
-    val height = raster.getHeight
-    val width = raster.getWidth
-    println("Height: " + height, " Width: " + width)
-    val features : Array[Double] = new Array[Double](height*width)
+
+    // resize original 280x280 image to 28x28
+    val width = 28
+    val height = 28
+    val image: BufferedImage = ImageIO.read(new File("src/main/resources/data/out.png"))
+    val scaleImage = image.getScaledInstance(width, height, Image.SCALE_DEFAULT)
+    val bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+    bufferedImage.getGraphics().drawImage(scaleImage, 0, 0 , null);
+    val raster: Raster  = bufferedImage.getRaster()
+
+    // save gray scale pixel into feature array
+    val feature : Array[Double] = new Array[Double](height*width)
     for (j <- 0 to (height-1) ) {
       for(i <- 0 to (width-1)) {
         val curPixel = raster.getSample(i,j,0)
         print(Integer.toString(curPixel)+",")
-        features(j*height + i) = curPixel.toDouble
+        feature(j*height + i) = curPixel.toDouble
       }
       println()
     }
 
+    // generate testing data's feature
+    val testing = Vectors.dense(feature)
+
+    // load trained model and do prediction
     val lrLBFGSModelPath = "src/main/resources/model/lrLBFGS"
     val lrLBFGSModel = LogisticRegressionModel.load(sc, lrLBFGSModelPath)
-    val testing = Vectors.dense(features)
-    //println(testing.size)
     val predictions = lrLBFGSModel.predict(testing)
     println("The digit you wrote was: " + predictions.toInt)
 
